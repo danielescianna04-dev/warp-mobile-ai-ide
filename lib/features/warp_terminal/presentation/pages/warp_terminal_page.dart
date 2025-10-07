c28a628e07ae10cc245d4923ac674024dfa77faa
danieleair <daniele.scianna04@gmail.com>
2025-10-08 00:36:12 +0200

    Reduce top/bottom padding to 6px for tighter spacing

--- lib/features/warp_terminal/presentation/pages/warp_terminal_page.dart
+++ lib/features/warp_terminal/presentation/pages/warp_terminal_page.dart
@@ -1823,7 +1823,7 @@ class _WarpTerminalPageState extends State<WarpTerminalPage> with TickerProvider
           child: ClipRRect(
             borderRadius: BorderRadius.circular(16),
             child: ExpansionTile(
-              tilePadding: const EdgeInsets.fromLTRB(18, 10, 18, 10),
+              tilePadding: const EdgeInsets.fromLTRB(18, 6, 18, 6),
               childrenPadding: const EdgeInsets.fromLTRB(18, 0, 18, 18),
               title: SelectableText.rich(
                 TextSpan(
